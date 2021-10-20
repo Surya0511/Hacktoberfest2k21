1d30efc35a1b6a69554f4bbf4e56924c6facba90
Dushyantha Thilakarathne <dushyanthaat@gmail.com>
2021-10-20 15:02:42 +0530

    Fibonacci Series.c
    I have updated this code to print the Fibonacci series of 0 and 1.

--- Fibonacci Series.c
+++ Fibonacci Series.c
@@ -3,14 +3,18 @@ int main()
 {    
  int n1=0,n2=1,n3,i,number;    
  printf("Enter the number of elements:");    
- scanf("%d",&number);    
- printf("\n%d %d",n1,n2);//printing 0 and 1    
- for(i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  printf(" %d",n3);    
-  n1=n2;    
-  n2=n3;    
- }  
+ scanf("%d",&number);
+ if(number<2){
+ 	printf("%d",number);//if number =0 or 1, then print number as output
+ }else{
+ 	printf("\n%d %d",n1,n2);//printing 0 and 1    
+ 	for(i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed    
+ 	{    
+  		n3=n1+n2;    
+  		printf(" %d",n3);    
+  		n1=n2;    
+  		n2=n3;    
+ 	}  	
+ }
   return 0;  
- }    
+ }
