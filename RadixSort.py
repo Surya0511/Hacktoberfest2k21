8d31669624aa796499804f5e633469c1b128d28c
Phani2112 <93088271+Phani2112@users.noreply.github.com>
2021-10-24 20:47:19 +0530

    Update RadixSort.py

--- RadixSort.py
+++ RadixSort.py
@@ -42,6 +42,7 @@ def radixSort(arr):
   
     # Find the maximum number to know number of digits
     max1 = max(arr)
+Print("process")
   
     # Do counting sort for every digit. Note that instead
     # of passing digit number, exp is passed. exp is 10^i
